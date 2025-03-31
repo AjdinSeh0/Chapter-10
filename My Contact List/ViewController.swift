b203139a5333d47ac27d7be77a65fe9fced96b45
Ajdin <ajdinseho@hotmail.com>
2025-03-31 13:45:35 -0400

    Initial Commit

---
+++ My Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Contact List
+//
+//  Created by Ajdin Seho on 3/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ My Contact ListTests/My_Contact_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Contact_ListTests.swift
+//  My Contact ListTests
+//
+//  Created by Ajdin Seho on 3/31/25.
+//
+
+import Testing
+@testable import My_Contact_List
+
+struct My_Contact_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
